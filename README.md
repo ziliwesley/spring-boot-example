20c92c014f2aaef6b43d436bc3f76d23d8b49cb4
Zili <ziliwesley@live.com>
2018-09-25 02:28:18 +0800

    Initialize the project

---
+++ README.md
@@ -0,0 +1,13 @@
+# Example project using Spring Boot
+
+- Spring Boot
+- docker-compose
+- mysql
+- nginx
+- Redis
+
+
+## References:
+
+- [sprint-boot-example](https://github.com/ityouknow/spring-boot-examples/tree/master/dockercompose-springboot-mysql-nginx/)
+- [Spring Boot 2.0(五)：Docker Compose + Spring Boot + Nginx + Mysql 实践](http://www.cnblogs.com/ityouknow/p/8661644.html)
---
+++ app/src/test/java/com/ziliwesley/SpringBootDemoApp/SpringBootDemoAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ziliwesley.SpringBootDemoApp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootDemoAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
